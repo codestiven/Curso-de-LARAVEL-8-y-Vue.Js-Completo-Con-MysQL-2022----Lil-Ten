a8eabf8e8be4373b8f13ba4d89bca4300a13733d
Stiven de la rosa <108486081+codestiven@users.noreply.github.com>
2024-06-25 22:55:24 -0400

    Listar Empleados CLASE - 9

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Empleado;
+
+
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
 route::get('/empleados', function () {
-    return 'listas de enpleados';
+    $empleados = Empleado::get();
+    return $empleados;
 });
