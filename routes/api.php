0e607a1794cbf024d0572b52ef6c4bcad9ef9184
Stiven de la rosa <108486081+codestiven@users.noreply.github.com>
2024-06-26 11:45:50 -0400

    Guardar Empleados - 11

--- routes/api.php
+++ routes/api.php
@@ -16,5 +16,15 @@ route::get('/empleados', function () {
 });
 
 route::post('/empleados' , function(Request $request){
-    return $request;
+    $empleados = new Empleado();
+    $empleados -> nombre = $request -> input('nombre');
+    $empleados -> apellidos = $request -> input('apellidos');
+    $empleados -> cedula = $request -> input('cedula');
+    $empleados -> email = $request -> input('email');
+    $empleados -> lugar_nacimiento = $request -> input('lugar_nacimiento');
+    $empleados -> genero = $request -> input('genero');
+    $empleados -> estado_civil = $request -> input('estado_civil');
+    $empleados -> telefono = $request -> input('telefono');
+    $empleados -> save();
+    return "empleado creado";
 });
